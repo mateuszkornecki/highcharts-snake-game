eeaba3c68f22d06b67a8488db6e75eb7d8c0eb58
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2020-05-19 12:18:23 +0200

    feat: changed the boundaries of the walls

--- js/Snake.js
+++ js/Snake.js
@@ -133,9 +133,9 @@ class Snake {
       case 'UP':
         return this.translateY - this.size >= 0;
       case 'DOWN':
-        return this.translateY + this.size <= this.chart.plotHeight;
+        return this.translateY + 2 * this.size <= this.chart.plotHeight;
       case 'RIGHT':
-        return this.translateX + this.size <= this.chart.plotWidth;
+        return this.translateX + 2 * this.size <= this.chart.plotWidth;
       case 'LEFT':
         return this.translateX - this.size >= 0;
     }
