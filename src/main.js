9b9df1bc269324704a6336ee64b32bc18cc1155a
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2020-05-25 16:58:25 +0200

    feat: removed polyfill for addEventListener()

--- src/main.js
+++ src/main.js
@@ -46,15 +46,12 @@ Highcharts.chart('game-container', {
 });
 
 /* eslint-disable */
-
 if (!Math.hypot) {
-  // eslint-disable-next-line no-unused-vars
   Math.hypot = function (x, y) {
     // https://bugzilla.mozilla.org/show_bug.cgi?id=896264#c28
     let max = 0;
     let s = 0;
     for (let i = 0; i < arguments.length; i += 1) {
-      // eslint-disable-next-line prefer-rest-params
       const arg = Math.abs(Number(arguments[i]));
       if (arg > max) {
         s *= (max / arg) * (max / arg);
@@ -65,34 +62,3 @@ if (!Math.hypot) {
     return max === 1 / 0 ? 1 / 0 : max * Math.sqrt(s);
   };
 }
-
-// addEventListener polyfill 1.0 / Eirik Backer / MIT Licence
-(function (win, doc) {
-  if (win.addEventListener) return;		// No need to polyfill
-
-  function docHijack(p) { const old = doc[p]; doc[p] = function (v) { return addListen(old(v)); }; }
-  function addEvent(on, fn, self) {
-    return (self = this).attachEvent(`on${on}`, (e) => {
-      var e = e || win.event;
-      e.preventDefault = e.preventDefault || function () { e.returnValue = false; };
-      e.stopPropagation = e.stopPropagation || function () { e.cancelBubble = true; };
-      fn.call(self, e);
-    });
-  }
-  function addListen(obj, i) {
-    if (i = obj.length) while (i--)obj[i].addEventListener = addEvent;
-    else obj.addEventListener = addEvent;
-    return obj;
-  }
-
-  addListen([doc, win]);
-  if ('Element' in win)win.Element.prototype.addEventListener = addEvent;			// IE8
-  else {																			// IE < 8
-    doc.attachEvent('onreadystatechange', () => { addListen(doc.all); });		// Make sure we also init at domReady
-    docHijack('getElementsByTagName');
-    docHijack('getElementById');
-    docHijack('createElement');
-    addListen(doc.all);
-  }
-}(window, document));
-
